13a9fe8abddb6abce688f55766194331717c5437
Pradip Gharge <59740723+pradipgharge@users.noreply.github.com>
2021-08-16 02:11:12 +0530

    feat: added event listener

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
 var txtInput = document.querySelector("#input-text");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output-text");
+
+btnTranslate.addEventListener("click", clickHandler);
